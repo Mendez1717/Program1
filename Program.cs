ba797236388eee2725e9ad616fe022ad2940dc15
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-02-06 20:31:07 -0500

    Version 2 to incorporate letter grade change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace Program1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int gradescore;
+            Console.WriteLine("Please enter your in your grade out of 100 then hit enter");
+            gradescore = Convert.ToInt32(Console.ReadLine());
+
+            if (gradescore >= 100 && gradescore >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (gradescore >= 80 && gradescore <= 89)
+            {
+                Console.WriteLine("B");
+            }
+            else if (gradescore >= 70 && gradescore <= 79)
+            {
+                Console.WriteLine("C");
+            }
+            else if (gradescore >= 60 && gradescore <= 69)
+            {
+                Console.WriteLine("D");
+            }
+            else if (gradescore <= 59 && gradescore >= 0)
+            {
+                Console.WriteLine("F");
+            }
+        
         }
     }
 }
